dbca4744534bf38cd5e8608b078ad1c1ab49df19
trikoh94 <trikoh94@gmail.com>
2024-10-29 20:55:21 +0900

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3394988771"
+    serviceWorkerVersion: "1662703289"
   }
 });
